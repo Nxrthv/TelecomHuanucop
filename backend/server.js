140db410d7f955107455aebf563becbcb9196bd7
SISTEMAS02 <dameda97@gmail.com>
2024-12-31 09:41:10 -0500

    MedinaDaza

--- backend/server.js
+++ backend/server.js
@@ -171,7 +171,7 @@ app.use((err, req, res, next) => {
   res.status(500).send('¡Algo salió mal!');
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en puerto ${PORT}`);
   console.log(`-----------------------------------------------------------`);
